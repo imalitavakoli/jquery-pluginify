1867db0e53ec1e22396349cab6fd5c6dacf44447
Ali <imalitavakoli@users.noreply.github.com>
2023-06-28 14:02:13 -0400

    updated Babelify dep.

--- dist/scripts/jp-all.js
+++ dist/scripts/jp-all.js
@@ -1,22 +1,17 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-'use strict';
+"use strict";
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-var _pluginify = require('../pluginify');
-
-var _pluginify2 = _interopRequireDefault(_pluginify);
-
-var _utils = require('../utils/utils');
-
-var utils = _interopRequireWildcard(_utils);
-
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
+var _pluginify = _interopRequireDefault(require("../pluginify"));
+var utils = _interopRequireWildcard(require("../utils/utils"));
+function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
+function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 /**
  * jp-plugin-name - jQueryPluginify sample block
  * @author 2023 ImAliTavakoli
@@ -78,33 +73,15 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
  * // The following events will be triggered on special occasions
  * $('.jp-plugin-name').on('statusChanged.gppluginname', (e, param) => { console.log(param.status); });
  */
-var PluginName = function () {
-  _createClass(PluginName, [{
-    key: '_properties',
-
-
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    /// Properties
-
-    value: function _properties(element, options) {
-      this.$element = $(element);
-      this.options = this._getOptions(options);
-    }
-
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////////////////////////////////////////////
-    /// Constructor
-
-  }]);
+var PluginName = /*#__PURE__*/function () {
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////
+  /// Constructor
 
   function PluginName(element, options) {
     _classCallCheck(this, PluginName);
-
     this._properties(element, options);
   }
 
@@ -113,29 +90,41 @@ var PluginName = function () {
   //////////////////////////////////////////////////////////////////////////////
   //////////////////////////////////////////////////////////////////////////////
   /// Static constant
-
   _createClass(PluginName, [{
-    key: '_getOptions',
+    key: "_properties",
+    value:
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    /// Properties
 
-
+    function _properties(element, options) {
+      this.$element = $(element);
+      this.options = this._getOptions(options);
+    }
+  }, {
+    key: "_getOptions",
+    value:
     //////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////
     /// Private functions
 
-    value: function _getOptions(options) {
+    function _getOptions(options) {
       // The following options should be boolean. Now if the user mentions them
       // via data API without specifying their value, then they will be string, so
       // we check that if they are string, let's convert them into boolean.
       options.jpDisabled = utils.toBoolean(options.jpDisabled);
-
       return options;
     }
   }, {
-    key: '_dispatchEvent',
+    key: "_dispatchEvent",
     value: function _dispatchEvent(status) {
-      this.$element.trigger($.Event('statusChanged.gppluginname'), { status: status });
+      this.$element.trigger($.Event('statusChanged.gppluginname'), {
+        status: status
+      });
     }
 
     //////////////////////////////////////////////////////////////////////////////
@@ -143,9 +132,8 @@ var PluginName = function () {
     //////////////////////////////////////////////////////////////////////////////
     //////////////////////////////////////////////////////////////////////////////
     /// Methods
-
   }, {
-    key: 'toggleDisability',
+    key: "toggleDisability",
     value: function toggleDisability() {
       if (this.$element.hasClass(this.options.jpDisabledClass)) {
         this.$element.removeClass(this.options.jpDisabledClass);
@@ -156,10 +144,9 @@ var PluginName = function () {
       }
     }
   }, {
-    key: 'destroy',
+    key: "destroy",
     value: function destroy() {
       var keepCssStyles = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
-
       // Remove all probable added styles from any element
       if (!keepCssStyles) {
         this.$element.removeClass(this.options.jpDisabledClass);
@@ -170,7 +157,7 @@ var PluginName = function () {
       this.options = undefined;
     }
   }], [{
-    key: 'DEFAULTS',
+    key: "DEFAULTS",
     get: function get() {
       return {
         jpDisabledClass: 'jp-plugin-name--state-disabled',
@@ -178,14 +165,9 @@ var PluginName = function () {
       };
     }
   }]);
-
   return PluginName;
-}();
-
-// Create our jQuery plugin
-
-
-(0, _pluginify2.default)('gppluginname', PluginName);
+}(); // Create our jQuery plugin
+(0, _pluginify["default"])('gppluginname', PluginName);
 
 // If there's no initializeJPBlocksManually variable which is true, then run the
 // plugin for all the blocks in the page
@@ -196,15 +178,13 @@ if (typeof window.initializeJPBlocksManually !== 'boolean' || window.initializeJ
 }
 
 },{"../pluginify":2,"../utils/utils":3}],2:[function(require,module,exports){
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
-
-exports.default = pluginify;
+exports["default"] = pluginify;
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
 /**
  * Generate a jQuery plugin.
  * Inspired by: https://gist.github.com/monkeymonk/c08cb040431f89f99928132ca221d647
@@ -251,8 +231,7 @@ exports.default = pluginify;
  */
 function pluginify(pluginName, className) {
   var shortHand = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
-
-  var dataName = '__' + pluginName;
+  var dataName = "__".concat(pluginName);
   var old = $.fn[pluginName];
 
   // Call our plugin for any number of DOM elements (the elements that our
@@ -260,10 +239,9 @@ function pluginify(pluginName, className) {
   // then return the results of .each() as it returns this (the original jQuery
   // object) to enable chaining.
   $.fn[pluginName] = function (option) {
-    for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
       args[_key - 1] = arguments[_key];
     }
-
     return this.each(function () {
       var $this = $(this);
       var data = $this.data(dataName);
@@ -271,8 +249,7 @@ function pluginify(pluginName, className) {
       // Let's merge all of the options that we've recevied from different
       // places: defaults, data API, and the options that user has passed while
       // calling the plugin.
-      var options = $.extend({}, className.DEFAULTS, $this.data(), (typeof option === 'undefined' ? 'undefined' : _typeof(option)) === 'object' && option);
-
+      var options = $.extend({}, className.DEFAULTS, $this.data(), _typeof(option) === 'object' && option);
       if (!data) {
         $this.data(dataName, data = new className(this, options));
       }
@@ -310,14 +287,14 @@ function pluginify(pluginName, className) {
 }
 
 },{}],3:[function(require,module,exports){
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.toBoolean = toBoolean;
 exports.inlineStyleValue = inlineStyleValue;
 exports.removeInlineStyles = removeInlineStyles;
+exports.toBoolean = toBoolean;
 /**
  * utils - Some helper functions
  * @author 2023 ImAliTavakoli
@@ -349,11 +326,8 @@ exports.removeInlineStyles = removeInlineStyles;
 function toBoolean(param) {
   var checkString = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
   var returnVarIfNotSupported = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : false;
-
   if (typeof param == 'boolean') return param;
-
   if (typeof param == 'number' && param > 0) return true;
-
   if (typeof param == 'string') {
     if (checkString) {
       if (param.toUpperCase() === 'TRUE' || param.toUpperCase() === 'ON' || param.toUpperCase() === 'YES' || param === '1') return true;
@@ -361,7 +335,6 @@ function toBoolean(param) {
       return true;
     }
   }
-
   if (returnVarIfNotSupported) return param;else return false;
 }
 
@@ -376,12 +349,10 @@ function toBoolean(param) {
  */
 function inlineStyleValue($element, param) {
   var styles = $element.attr('style'),
-      value = void 0;
-
+    value;
   if (styles !== undefined) {
     styles.split(';').forEach(function (e) {
       var style = e.split(':');
-
       if ($.trim(style[0]) === param) {
         value = style[1];
         value = value.replace(/\s/g, ''); // remove any possible spaces in the value
@@ -400,10 +371,9 @@ function inlineStyleValue($element, param) {
  * @return {String}
  */
 function removeInlineStyles($element) {
-  for (var _len = arguments.length, params = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+  for (var _len = arguments.length, params = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
     params[_key - 1] = arguments[_key];
   }
-
   if ($element.attr('style') !== undefined) {
     $element.attr('style', function (j, style) {
       var regex = new RegExp('(' + params.join('|') + ')\s?:\s?[^;]+;?', 'g');
@@ -414,8 +384,8 @@ function removeInlineStyles($element) {
 }
 
 },{}],4:[function(require,module,exports){
-'use strict';
+"use strict";
 
-require('../blocks/plugin-name/plugin-name');
+require("../blocks/plugin-name/plugin-name");
 
 },{"../blocks/plugin-name/plugin-name":1}]},{},[4]);
